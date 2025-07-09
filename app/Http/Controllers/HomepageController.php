297fb8b7fb7f4ecece8b87722603fef9e3248571
Your Name <you@example.com>
2025-07-09 14:44:22 +0700

    Update

--- app/Http/Controllers/HomepageController.php
+++ app/Http/Controllers/HomepageController.php
@@ -27,13 +27,12 @@ class HomepageController extends Controller
     public function index()
     {
         $categories = Categories::latest()->take(4)->get();
-        $products = Product::where('is_active', true)->latest()->take(20)->get();
-        // $products = Product::paginate(20);
+        $products = Product::paginate(20);
         
-    return view("theme.{$this->themeFolder}.homepage", [
-            'title' =>   'Homepage',
-            'categories' =>     $categories,
-            'products'=>    $products,
+        return view($this->themeFolder.'.homepage',[
+            'categories' => $categories,
+            'products'=>$products,
+            'title'=>'Homepage'
         ]);
     }
 
